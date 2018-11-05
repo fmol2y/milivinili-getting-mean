7b60eaecc5c98a04a3143e258f8e35b3c0300155
Filip Moldvai <filip.moldvai@devoteam.com>
2018-11-05 17:02:11 +0100

    started adding routes

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const ctrlMain = require('../controllers/main');
+const ctrlUser = require('../controllers/userController');
+const ctrlOthers = require('../controllers/othersController');
+const ctrlVinylsArtists = require('../controllers/vinylArtistController');
+const ctrlExchange = require('../controllers/exchangeController');
+const ctrlHome = require('../controllers/homeController');
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlHome.index);
+
+/* Vinyl & artist related pages */
+router.get('/vinyls', ctrlVinylsArtists.vinylList);
+router.get('/vinyls/vinyl', ctrlVinylsArtists.vinylInfo);
+router.get('/artists', ctrlVinylsArtists.artistInfo);
+router.get('/artists/artist', ctrlVinylsArtists.artistInfo);
+
+/* Exchange related pages */
+router.get('/exchanges', ctrlExchange.exchangeList);
+router.get('/exchanges/exchange', ctrlExchange.exchangeInfo);
+
+/* Other pages */
+router.get('/about', ctrlOthers.about);
 module.exports = router;
